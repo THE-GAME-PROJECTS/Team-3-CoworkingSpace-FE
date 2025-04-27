9b99c3ec414f7401ba70028f46fdf7536ca63b21
Artem Yavorskyi <uayoungin@gmail.com>
2025-04-27 08:42:43 +0300

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,3 @@ export default {
   },
   plugins: [],
 };
-module.exports = {
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        ".scrollbar-overlay": {
-          "scrollbar-gutter": "auto",
-          "&::-webkit-scrollbar": {
-            width: "8px",
-            position: "fixed",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            backgroundColor: "#888",
-            borderRadius: "4px",
-          },
-        },
-      });
-    },
-  ],
-};
